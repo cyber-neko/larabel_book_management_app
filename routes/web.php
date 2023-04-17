5f243086e38b4035a59e4fdca331a81dca9ecf06
Cyber Neko <firexneko7@gmail.com>
2023-04-17 23:57:27 +0900

    chapter10: 本の登録処理の作成 完了

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Book;
 
 /*
@@ -32,7 +33,29 @@ Route::get('/', function () {
  * 本を追加 
  */
 Route::post('/books', function (Request $request) {
-    //
+    //バリデーション
+    //ルールを記載する
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255|min3',
+    ]);
+
+    //バリデーション:エラー 
+    //withInput(): セッションに入力値を保存
+    //エラー内容を$error変数に渡す
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // Eloquentモデル（登録処理）
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
 });
 
 /**
