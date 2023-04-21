b8bdd032da6afb8d75e672e0d26e600edadb8cf2
Cyber Neko <firexneko7@gmail.com>
2023-04-22 00:12:34 +0900

    chapter14: ページネーションの使用 完了

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -11,7 +11,7 @@ class BooksController extends Controller
 {
     public function index()
     {
-        $books = Book::orderBy('created_at', 'asc')->get();
+        $books = Book::orderBy('created_at', 'asc')->paginate(3);
         // view関数は第2引数に使用するデータを配列で渡す
         return view('books', [
             'books' => $books
